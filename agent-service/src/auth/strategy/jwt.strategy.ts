5e7c2fac216a6bf5ec2353da149d44e30ca2382b
WenhaoYou1 <ericwyou1@gmail.com>
2025-02-18 02:47:10 -0500

    fix(agents): fix the JWT auth

--- agent-service/src/auth/strategy/jwt.strategy.ts
+++ agent-service/src/auth/strategy/jwt.strategy.ts
@@ -1,12 +1,8 @@
+// src/auth/strategy/jwt.strategy.ts
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
-
-interface JwtPayload {
-  sub: number;
-  username: string;
-}
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -16,11 +12,12 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('JWT_SECRET') || 'default_secret',
+      secretOrKey: configService.get<string>('JWT_SECRET') || 'secretKey',
     });
   }
 
-  validate(payload: JwtPayload): { userId: number; username: string } {
+  validate(payload: any) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
     return { userId: payload.sub, username: payload.username };
   }
 }
